ac59b13a1410ba84848b3cba47ba95e06b4bcdad
jiayangli2 <jl4305@columbia.edu>
2017-04-07 16:54:29 -0400

    deleted

--- tweepy_and_worker/alchemy.py
+++ tweepy_and_worker/alchemy.py
@@ -2,17 +2,37 @@ import boto.sqs
 from boto.sqs.message import Message
 from watson_developer_cloud import AlchemyLanguageV1
 import json
+from multiprocessing.dummy import Pool as ThreadPool 
 
 conn = boto.sqs.connect_to_region("us-east-2")
 my_queue = conn.get_queue('twittmap')
 
-rs = my_queue.get_messages()
-m=rs[0]
-body=m.get_body()
-toa = json.loads(body)
+def get_msg():
+    rs = my_queue.get_messages()
+    m=rs[0]
+    body=m.get_body()
+    my_queue.delete_message(m)
+    toa = json.loads(body)
 
-alchemy_language = AlchemyLanguageV1(api_key='e84c6beb272209974c54c2b61af62699963ecc9a')
-result = alchemy_language.sentiment(text=toa['text'])
-print(result[''])
-print(result['docSentiment'])
-sentiment = result['docSentiment']['type']
+    alchemy_language = AlchemyLanguageV1(api_key='e84c6beb272209974c54c2b61af62699963ecc9a')
+    result = alchemy_language.sentiment(text=toa['text'])
+    #print(toa['text'])
+    #print(result['docSentiment'])
+    sentiment = result['docSentiment']['type']
+    #print(sentiment)
+
+    response = client.publish(
+        TopicArn='twittmap',
+        TargetArn='arn:aws:sns:us-east-2:851980283070:twittmap',
+        #PhoneNumber='string',
+        Message=toa,
+        Subject='string',
+        MessageStructure='json',
+        MessageAttributes={
+            'string': {
+                'DataType': 'string',
+                'StringValue': 'string',
+                'BinaryValue': b'bytes'
+            }
+        }
+    )
