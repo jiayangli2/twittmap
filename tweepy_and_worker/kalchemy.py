6503f919914e8ec0abfac3baf9b401a8cda3d78a
EC2 Default User <ec2-user@ip-172-31-5-160.us-west-2.compute.internal>
2017-04-09 18:37:44 +0000

    k-version

---
+++ tweepy_and_worker/kalchemy.py
@@ -0,0 +1,44 @@
+import boto3
+from watson_developer_cloud import AlchemyLanguageV1
+import json
+from threading import Thread
+
+def get_msg(i): 
+     while(True):
+        response=sqs.receive_message(QueueUrl = 'https://sqs.us-east-2.amazonaws.com/851980283070/twittmap')
+        if(response==None):
+            continue
+        print "ko"
+        if not ('Messages' in response): 
+            continue
+        t=response['Messages']
+        toa = json.loads(t[0]['Body'])
+        receipt_handle = t[0]['ReceiptHandle']
+        sqs.delete_message(QueueUrl='https://sqs.us-east-2.amazonaws.com/851980283070/twittmap', ReceiptHandle=receipt_handle)
+        alchemy_language = AlchemyLanguageV1(api_key='e84c6beb272209974c54c2b61af62699963ecc9a')
+        result = alchemy_language.sentiment(text=toa['text'])
+        #print(toa['text'])
+        #print(result['docSentiment'])
+        sentiment = result['docSentiment']['type']
+        #toa['sentiment']=sentiment
+        toa['geo_location'] = {"lat":toa['place']['bounding_box']['coordinates'][0][0][1], "lon":toa['place']['bounding_box']['coordinates'][0][0][0]}
+        tbs = {"text":toa['text'], "sentiment":sentiment, "geo_location":toa['geo_location'],}
+        to_be_sent = json.dumps({"default":json.dumps(tbs)})
+        # print len(to_be_sent)
+        #to_be_sent = json.dumps(to_be_sent)
+        response = client.publish(
+            TopicArn='arn:aws:sns:us-east-2:851980283070:twittmap',
+            Message = to_be_sent,
+            Subject='PNNTwitt',
+            MessageStructure='json'
+        )
+        continue
+sqs = boto3.client('sqs', region_name='us-east-2')
+client = boto3.client('sns', region_name='us-east-2')
+
+for i in range(5):
+    t=Thread(target=get_msg, args=(i, ))
+    t.setDaemon(True)
+    t.start()
+
+
---
+++ tweepy_and_worker/kstream.py
@@ -0,0 +1,48 @@
+from __future__ import absolute_import, print_function
+import tweepy
+import json
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
+import boto3
+
+
+
+consumer_key='GdeEd2wSLwPE3dFBBX7ZxIvzN'
+consumer_secret='x2fpqVxPtEOjyO4ct3WqqXIfMhLMswkrdNdYUIqKgpQ6BqyW8n'
+access_token='178733793-9cQ3d0dqc9Uf6CHIxdwjYpFmdrkfNAtjCQOkSGpf'  
+access_token_secret='1k1ZVULtD6msZgTqPjAeD0fudeMYiCuWD9XpWolKS6z2y'
+
+
+class StdOutListener(StreamListener):
+    def __init__(self, api=None):
+        super(StdOutListener, self).__init__()
+        self.tweet=[]
+        self.count=0
+        self.sqs=boto3.resource('sqs', region_name="us-east-2")
+        self.my_queue = self.sqs.get_queue_by_name(QueueName='twittmap')
+    def on_data(self, data):
+        if data != None:
+            temp = json.loads(data)
+            #print(temp['place'])
+            if 'place' in temp and temp['place']!=None and temp['place']['bounding_box']['coordinates'][0][0] != None and temp['lang']=='en':
+                self.count+=1
+                print (1)
+                response=self.my_queue.send_message(MessageBody=data)
+            if self.count <= 100:
+                return True
+            else:
+                return False
+
+    def on_error(self, status):
+        print(status)
+
+l = StdOutListener()
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)  
+auth.set_access_token(access_token, access_token_secret)
+
+stream = Stream(auth, l)
+stream.filter(track=['hot'])
+
+
+
